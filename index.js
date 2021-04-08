9ced716015445e7ea0118a0066afbc3d51994a46
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-08 13:02:10 -0400

    commit tree 77d3ddbb2d69626d8fcc784e94adfc1f0461f6bd

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+let obj = [];
+let titlekey = [];
+function titleCased(x) 
+{
+  for (const num of tutorials) {    // this iterates through each element of tutorials
+    obj = num.split(" ");    // takes each element and turns it into an array obj
+    obj.forEach(function(element,index,array) //iterates through each element of the array obj
+    {    
+      let l = element.charAt(0).toUpperCase(); // this sets the first character in each element to uppercase
+      let k =  element.slice(1);
+      array[index] = l + k;
+    })
+    titlekey.push(obj.join(" "));
+  }
+  //console.log(titlekey)
+  return titlekey
 }
+
+
+
+
+
+
